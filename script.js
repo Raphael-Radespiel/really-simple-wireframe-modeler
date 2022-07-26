5d2e499ee1689025954b9908866a01247a2522dd
Raphael Radespiel <contatoraphael1701@gmail.com>
2022-07-26 10:52:14 -0300

    Switch wireframe input to textarea using .obj

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-const removeVerticeBtn = document.querySelector('#vertice-remove'); 
-const addVerticeBtn = document.querySelector('#vertice-add'); 
-const removeEdgeBtn = document.querySelector('#edge-remove'); 
-const addEdgeBtn = document.querySelector('#edge-add'); 
-
-const verticeInputField = document.querySelector('.vertice-input-field').cloneNode(true);
-const edgeInputField = document.querySelector('.edge-input-field').cloneNode(true);
-
-const verticeList = document.querySelector('.vertice-list');
-const edgeList = document.querySelector('.edge-list');
-
-const verticeButtonContainer = document.querySelector('.vertice-list .button-container');
-const edgeButtonContainer = document.querySelector('.edge-list .button-container');
-
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 canvas.height *= 2;
@@ -30,12 +16,17 @@ let simpleCube = {
   [1,5], [2, 6], [3, 7], [4, 8]],
 }
 
+let validWiremesh = {
+  vertices: {},
+  edges: {},
+};
+
 function getEmptyMatrix(){
   return [[0,0,0,0],
           [0,0,0,0],
           [0,0,0,0],
           [0,0,0,0]];
-}
+};
 
 let projectionMatrix = getEmptyMatrix();
 
@@ -47,79 +38,12 @@ projectionMatrix[2][3] = 1.0;
 
 let rotateXMatrix = getEmptyMatrix();
 let rotateZMatrix = getEmptyMatrix();
+let identityMatrix = getEmptyMatrix();
 
-function GraphicalObject(){
-  return{
-    amount: 1,
-    valueArray: [[]],
-    addArray(){
-      this.valueArray.push([]);
-    },
-    removeArray(){
-      this.valueArray.pop();
-    },
-    changeArray(i, j, value){
-      this.valueArray[i][j] = value;
-    },
-  };  
-}
-
-let vertexObj = new GraphicalObject;
-let edgeObj = new GraphicalObject;
-
-function handleTextInput(){
-  verticeList.addEventListener('change', (n) => {
-    let hashForVertex = {x: 0, y: 1, z: 2};
-    let target = n.target;
-    let index = target.parentNode.id.substr(target.parentNode.id.length - 1) - 1;
-    let value = target.value;
-    vertexObj.changeArray(index, hashForVertex[target.id], value);
-    console.log(vertexObj.valueArray);
-  });
-  
-  edgeList.addEventListener('change', (n) => {
-    let hashForEdge = {start: 0, end: 1};
-    let target = n.target;
-    let index = target.parentNode.id.substr(target.parentNode.id.length - 1) - 1;
-    let value = target.value;
-    edgeObj.changeArray(index, hashForEdge[target.id], value);
-    console.log(edgeObj.valueArray);
-  });
-}
-
-function initAddRemoveButtons(){
-  addVerticeBtn.addEventListener('click', () => {
-    addElement(vertexObj, verticeList, verticeInputField, verticeButtonContainer);
-  });
-  removeVerticeBtn.addEventListener('click', () => {
-    removeElement(vertexObj, verticeList);
-  });
-
-  addEdgeBtn.addEventListener('click', () => {
-    addElement(edgeObj, edgeList, edgeInputField, edgeButtonContainer)
-  });
-  removeEdgeBtn.addEventListener('click', () => {
-    removeElement(edgeObj, edgeList);
-  });
-}
-
-function addElement(obj, listElement, inputElement, buttonContainerElement){
-  let cloneOfInputField = inputElement.cloneNode(true);
-  obj.amount++;
-  obj.addArray();
-  cloneOfInputField.id = cloneOfInputField.id.substr(0, cloneOfInputField.id.length -1) + `${obj.amount}`;
-  cloneOfInputField.firstElementChild.textContent = `${obj.amount})`;
-  listElement.insertBefore(cloneOfInputField, buttonContainerElement);
-}
-
-function removeElement(obj, listElement){
-  if(obj.amount > 0){
-    let penultimateChild = listElement.querySelector('div:nth-last-child(2)');
-    obj.amount--;
-    obj.removeArray();
-    listElement.removeChild(penultimateChild);
-  }
-}
+identityMatrix[0][0] = 1;
+identityMatrix[1][1] = 1;
+identityMatrix[2][2] = 1;
+identityMatrix[3][3] = 1;
 
 function multiplyMatrixVector(i, m){
   let o = {
@@ -150,6 +74,20 @@ function drawLine(edge){
   context.moveTo(edge[0].x, edge[0].y);
   context.lineTo(edge[1].x, edge[1].y);
   context.stroke();
+}
+
+function matrixMultiplication(mat1, mat2){
+  let mat3 = getEmptyMatrix();
+
+  for(let i = 0; i < 4; i++){
+    for(let j = 0; j < 4; j++){
+      for(let k = 0; k < 4; k++){
+        mat3[i][j] += mat1[i][k] * mat2[k][j];
+      }
+    }
+  }
+  console.log(mat3);
+  return mat3;
 }
 
 function drawEdges(){
@@ -194,7 +132,6 @@ function drawEdges(){
 
     firstCoordinate.z += 3;
     secondCoordinate.z += 3;
-
     let projectedEdge = [multiplyMatrixVector(firstCoordinate, projectionMatrix),
       multiplyMatrixVector(secondCoordinate, projectionMatrix)];
 
@@ -214,9 +151,5 @@ function drawEdges(){
 
   setTimeout(drawEdges, 41);
 }
-
-initAddRemoveButtons();
-
-handleTextInput();
 
 drawEdges();
